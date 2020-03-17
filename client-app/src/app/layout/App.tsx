2b173e00d9f6a6e3accf8b4dc47ec96fed8f6bc0
dmoriley <david.m.oriley@hotmail.com>
2020-03-16 22:38:34 -0400

    Crud for front end in place

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,31 +1,69 @@
-import React, { useState, useEffect } from "react";
-import { Header, Icon, List } from "semantic-ui-react";
-import { IActivity } from "../models/activity";
 import axios from "axios";
+import React, { Fragment, useEffect, useState } from "react";
+import { Container } from "semantic-ui-react";
+import ActivityDashboard from "../../features/activities/dashboard/ActivityDashboard";
 import NavBar from "../../features/nav/NavBar";
-
-// interface IState {
-//   activities: IActivity[];
-// }
+import { IActivity } from "../models/activity";
 
 const App = () => {
   const [activities, setActivities] = useState<IActivity[]>([]);
+  const [selectedActivity, setSelectedActivity] = useState<IActivity | null>(null);
+  const [editMode, setEditMode] = useState(false);
+
+  const handleSelectActivity = (id: string) => {
+    setSelectedActivity(activities.filter(i => i.id === id)[0]);
+    setEditMode(false);
+  };
+
+  const handleOpenCreateForm = () => {
+    setSelectedActivity(null);
+     setEditMode(true);
+  };
+
+  const handleCreateActivity = (activity: IActivity) => {
+    setActivities([...activities, activity]);
+    setEditMode(false);
+    setSelectedActivity(activity);
+  }
+
+  const handleEditActivity = (activity: IActivity) => {
+    setActivities([...activities.filter(a => a.id !== activity.id), activity]);
+    setEditMode(false);
+    setSelectedActivity(activity);
+  }
+
+  const handleDeleteActivity = (id: string) => {
+    setActivities([...activities.filter(a => a.id !== id)])
+  }
 
   useEffect(() => {
     axios.get<IActivity[]>("http://localhost:5000/api/activities").then(res => {
-      setActivities(res.data);
+      let activities: IActivity[] = [];
+      res.data.forEach(activity => {
+        activity.date = activity.date.split('.')[0];
+        activities.push(activity);
+      });
+      setActivities(activities);
     });
   }, []);
 
   return (
-    <div>
-      <NavBar />
-      <List>
-        {activities.map(activity => (
-          <List.Item key={activity.id}>{activity.title}</List.Item>
-        ))}
-      </List>
-    </div>
+    <Fragment>
+      <NavBar openCreateForm={handleOpenCreateForm}/>
+      <Container style={{ marginTop: "7em" }}>
+        <ActivityDashboard
+          activities={activities}
+          selectActivity={handleSelectActivity}
+          selectedActivity={selectedActivity}
+          editMode={editMode}
+          setEditMode={setEditMode}
+          setSelectedActivity={setSelectedActivity}
+          createActivity={handleCreateActivity}
+          editActivity={handleEditActivity}
+          deleteActivity={handleDeleteActivity}
+        />
+      </Container>
+    </Fragment>
   );
 };
 
---
+++ client-app/src/features/activities/form/ActivityForm.tsx
@@ -0,0 +1,109 @@
+import React, { useState, FormEvent } from 'react';
+import { Segment, Form, Button } from 'semantic-ui-react';
+import { IActivity } from '../../../app/models/activity';
+import { v4 as uuid } from 'uuid';
+
+interface IProps {
+  setEditMode: (val: boolean) => void;
+  activity: IActivity | null;
+  createActivity: (activity: IActivity) => void;
+  editActivity: (activity: IActivity) => void;
+}
+
+const ActivityForm: React.FC<IProps> = ({
+  setEditMode,
+  activity: initialFormActivity,
+  createActivity,
+  editActivity,
+}) => {
+  const initializeForm = () => {
+    if (initialFormActivity) {
+      return initialFormActivity;
+    }
+
+    return {
+      id: '',
+      title: '',
+      category: '',
+      description: '',
+      date: '',
+      city: '',
+      venue: ''
+    };
+  };
+
+  const [activity, setActivity] = useState<IActivity>(initializeForm);
+
+  const handleSubmit = () => {
+    if(activity.id.length === 0) {
+      let newActivity  = {
+        ...activity,
+        id: uuid()
+      };
+      createActivity(newActivity);
+    } else {
+      editActivity(activity);
+    }
+  }
+
+  // input can also send FormEvents instead of change events. Cause the text area only sends form events we'll just
+  // use the form event. instead of event.target, a form event is event.currentTarget
+  const handleInputChange = (event: FormEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+    const { name, value } = event.currentTarget;
+    setActivity({ ...activity, [name]: value });
+  };
+
+  return (
+    <Segment clearing>
+      <Form onSubmit={handleSubmit}>
+        <Form.Input
+          placeholder="Title"
+          value={activity.title}
+          name="title"
+          onChange={handleInputChange}
+        />
+        <Form.TextArea
+          rows={2}
+          placeholder="Description"
+          value={activity.description}
+          name="description"
+          onChange={handleInputChange}
+        />
+        <Form.Input
+          placeholder="Category"
+          value={activity.category}
+          name="category"
+          onChange={handleInputChange}
+        />
+        <Form.Input
+          type="datetime-local"
+          placeholder="Date"
+          value={activity.date}
+          name="date"
+          onChange={handleInputChange}
+        />
+        <Form.Input
+          placeholder="City"
+          value={activity.city}
+          name="city"
+          onChange={handleInputChange}
+        />
+        <Form.Input
+          placeholder="Venue"
+          value={activity.venue}
+          name="venue"
+          onChange={handleInputChange}
+        />
+        <Button floated="right" positive type="submit" content="Submit" />
+        <Button
+          onClick={setEditMode.bind(null, false)}
+          floated="right"
+          type="button"
+          content="Cancel"
+        />
+      </Form>
+    </Segment>
+  );
+};
+
+export default ActivityForm;
